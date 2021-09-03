bf1fa8d22555aa09f0ab7a76aa9f11e79a24bed6
GhostClock <ghostingclock@gmail.com>
2021-09-03 15:43:28 +0800

    增加调试工程

---
+++ objc_test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  objc_test
+//
+//  Created by 张志远 on 2021/9/3.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
